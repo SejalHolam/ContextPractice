66d6694fe546f472b130325677cfd5ac5a8b821c
SejalHolam <sejal.sandeep.holam@gmail.com>
2026-02-19 20:43:02 +0530

    level 1 themeSwitcher

---
+++ themeSwitcher/contexts/ThemeContext.jsx
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+import { useState } from "react";
+import { createContext } from "react";
+
+export const ThemeContext = createContext({});
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState("dark");
+
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => {
+  return useContext(ThemeContext);
+};

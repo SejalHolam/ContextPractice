1c473bc58cc9164f738632a087ba6169b275e4fb
SejalHolam <sejal.sandeep.holam@gmail.com>
2026-02-20 02:06:48 +0530

    ui changes

--- Spotify_Theme_Mode/src/ThemeBtn.jsx
+++ Spotify_Theme_Mode/src/ThemeBtn.jsx
@@ -6,16 +6,29 @@ const ThemeBtn = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <button
-      onClick={toggleTheme}
-      className="p-3 rounded-full bg-gray-200 dark:bg-gray-800 transition"
-    >
-      {theme === "dark" ? (
-        <FaSun className="text-white" size={18} />
-      ) : (
-        <FaMoon className="text-black" size={18} />
-      )}
-    </button>
+    <label className="relative inline-flex items-center cursor-pointer">
+
+      <input
+        type="checkbox"
+        className="sr-only peer"
+        onChange={toggleTheme}
+        checked={theme === "dark"}
+      />
+
+      <div
+        className="w-11 h-6 bg-gray-200 rounded-full peer 
+        dark:bg-gray-700 peer-checked:bg-gray-800
+        after:content-[''] after:absolute after:top-[2px] after:left-[2px]
+        after:bg-white after:border after:rounded-full
+        after:h-5 after:w-5 after:transition-all
+        peer-checked:after:translate-x-full"
+      ></div>
+
+      <span className="ml-3 text-gray-900 dark:text-white">
+        {theme === "dark" ? <FaSun className="text-yellow-500" size={20} /> : <FaMoon size={20} />}
+      </span>
+
+    </label>
   );
 };
 
